c748bb86808a49568260340cb1e83fe84df105ba
unknown <grabbany9258>
2022-10-31 19:17:33 +0600

    class

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Model } from './repository.model';
 
 
 @Component({
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'example';
+  model: Model =new Model();
+ 
 }
---
+++ example/src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'header-area',
+    templateUrl: 'header.component.html'
+
+})
+
+export class HeaderComponent{
+
+    title = "My Header";
+
+}
