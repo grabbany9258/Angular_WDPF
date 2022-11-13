da35476055a6a9a7afd57be089ce6164f9028b07
unknown <grabbany9258>
2022-11-13 18:57:12 +0600

    angmodule project

---
+++ angmodules/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { StudentModule } from './student/student.module';
+import { StudentComponent } from './student/student/student.component';
+
+
+
+const routes: Routes = [
+  {
+      path: '',
+      component: StudentComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule , StudentModule , RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
